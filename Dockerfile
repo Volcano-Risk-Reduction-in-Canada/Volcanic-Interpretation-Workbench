097f36b97e0d99698fe60b4783f891f8760df7ae
drotheram <drew.rotheram-clarke@canada.ca>
2022-12-06 16:24:18 -0800

    feature: update coherence matrix with new data

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM python:3.9-slim
 
 COPY requirements.txt /tmp/
 COPY ./app /app
+COPY ./scripts /scripts
 WORKDIR "/app"
 
 RUN pip install -r /tmp/requirements.txt
---
+++ scripts/loadUpdatedCoherenceMatrix.py
@@ -0,0 +1,43 @@
+import argparse
+import boto3
+import configparser
+
+
+def main():
+    args = parse_args()
+    config = get_config_params("config.ini")
+
+    # Copy file from S3 into filesystem
+    s3 = boto3.client('s3')
+    s3.download_file(Bucket=config.get('AWS', 'bucketName'),
+                     Key=f'{args.site}/{args.beam}/CoherenceMatrix.csv',
+                     Filename=f'Data/{args.site}/{args.beam}/CoherenceMatrix.csv')
+
+
+def get_config_params(args):
+    """
+    Parse Input/Output columns from supplied *.ini file
+    """
+    configParseObj = configparser.ConfigParser()
+    configParseObj.read(args)
+    return configParseObj
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Copy latest coherence matrix into ")
+    parser.add_argument("--site",
+                        type=str,
+                        help="Volcano Site Name, i.e. 'Meager'",
+                        required=True)
+    parser.add_argument("--beam",
+                        type=str,
+                        help="RCM Beam Mode Mnemonic, i.e. '5M3'",
+                        required=True)
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == '__main__':
+    main()
