08d4c73cfc90060c2a73396da5bb75ca2014317b
Chloe Lam <chloelam2407@gmail.com>
2024-10-09 12:17:52 -0700

    fix pylint

--- scripts/get_latest_baselines.py
+++ scripts/get_latest_baselines.py
@@ -14,8 +14,9 @@ import os
 import botocore.exceptions
 import yaml
 
-from .data_utils import get_config_params
-from .global_variables import s3
+# from .data_utils import get_config_params
+# from .global_variables import s3
+from scripts_config import get_config_params, s3
 
 
 def get_latest_baselines():
--- scripts/global_variables.py
+++ scripts/global_variables.py
@@ -1,15 +1,15 @@
-#!/usr/bin/python3
-"""
-Volcano InSAR Interpretation Workbench
+# #!/usr/bin/python3
+# """
+# Volcano InSAR Interpretation Workbench
 
-SPDX-License-Identifier: MIT
+# SPDX-License-Identifier: MIT
 
-Copyright (C) 2021-2023 Government of Canada
+# Copyright (C) 2021-2023 Government of Canada
 
-Authors:
-  - Chloe Lam <chloe.lam@nrcan-rncan.gc.ca>
-"""
+# Authors:
+#   - Chloe Lam <chloe.lam@nrcan-rncan.gc.ca>
+# """
 
-import boto3
-# verify = False for when working in office to bypass SSL Certificate Error
-s3 = boto3.client('s3', verify=False)
+# import boto3
+# # verify = False for when working in office to bypass SSL Certificate Error
+# s3 = boto3.client('s3', verify=False)
